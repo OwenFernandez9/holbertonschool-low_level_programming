8f0877dd4f8aebe26303119e2a0d8aee6e84898e
Owen <9357@holbertonstudents.com>
2024-06-13 14:16:35 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main - jkshfsakjhs
- * Return: ksjfjabhdjfbh*
+ * Return: ksjfjabhdjfbh
  */
 
 int main(void)
 	{
 	puts("Programming is like building a multilingual puzzle");
-
 	return (0);
 }
