51b78e1be474f7c2151a3a3f4665ff87d0401dfc
root <9357@holbertonstudents.com>
2024-06-14 17:26:34 +0000

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int putchar(int char)
+	char ch;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
