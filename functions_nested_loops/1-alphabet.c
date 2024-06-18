4d2269b05cc8662b975d512457da925c4e9dbfb8
Owen <9357@holbertonstudents.com>
2024-06-18 01:53:02 +0000

    nose

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,11 +8,9 @@ void print_alphabet(void)
 	char abc = 'a';
 
 	while (abc <= 'z')
-
 	{
 		_putchar(abc);
 		abc++;
 	}
 	_putchar('\n');
-
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,2 @@
-#ifndef main_h
-#define main_h
-
-int _putchar(char);
-
-#endif
+int _putchar(char c);
+void print_alphabet(void);
