808f91d55fd5542e8dc06f2ab348a68ae2caf17e
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-18 13:50:13 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_alphabet_x10 - imprime el alfabeto en minuscula x10
  */
+
 void print_alphabet_x10(void)
 {
 	int count = 0;
@@ -14,6 +14,6 @@ void print_alphabet_x10(void)
 		{
 			_putchar(letter);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
