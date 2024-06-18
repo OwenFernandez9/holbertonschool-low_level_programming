2df885e691f40c11ad5ff3e18abc67bfaef8438f
Owen <9357@holbertonstudents.com>
2024-06-18 01:37:46 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - imprime el alfabeto en minuscula
+ */
+void print_alphabet(void)
+{
+	char abc = 'a';
+
+	while (abc <= 'z')
+
+	{
+		_putchar(abc);
+		abc++;
+	}
+	_putchar('\n');
+
+}
