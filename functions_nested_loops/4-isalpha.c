9f614ac1a431e1d9a520c4714eb53c8dd7d74e04
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-18 14:38:34 +0000

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - function is used to check lowercase letter
+ *
+ * @c: character check
+ *
+ * Return: c is 1 and letters is 0
+ */
+int _isalpha(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
