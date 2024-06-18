18f3ee64245e2f4a75c6fc93f965a7de1cb6fc8f
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-18 16:10:03 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - jehdue
+ *
+ * @n: numero
+ *
+ * Return: valor absoluto
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
