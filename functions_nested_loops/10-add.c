f83dcb15eda1a72a7c799f085eb86e2b01f2f546
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-18 18:11:26 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - aÃade dos integrales y retorna el resultado
+ * @number1: primer numero
+ * @number2: segundo numero
+ * Return: Resultado
+ */
+int add(int number1, int number2)
+{
+	return (number1 + number2);
+}   
