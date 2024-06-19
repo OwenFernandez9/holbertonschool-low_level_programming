8dbd77259ad90feb1bcae04dcaf1215e5f38bda0
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-19 18:12:26 +0000

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - intercambia las variables
+ *
+ * @a: variable 1
+ *
+ * @b: variable 2
+ */
+void swap_int(int *a, int *b)
+{
+	int c ;
+	c = *a
+	*a = *b
+	*b = c
+}
