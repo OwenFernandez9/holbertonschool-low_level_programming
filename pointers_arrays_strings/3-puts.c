ed4027c23e3bd82b45569e02275550d2f7e7a094
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-20 14:43:59 +0000

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - check the code
+ *
+ * @str: jaja
+ */
+void _puts(char *str)
+{
+	int lent;
+
+	for (lent = 0; str[lent] != '\0'; lent++)
+	{
+		_putchar(lent);
+	}
+}
