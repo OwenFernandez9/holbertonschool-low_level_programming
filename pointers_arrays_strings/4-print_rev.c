f114cf24f6865c1e579e339c78af49f8ff5c5b1b
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-20 18:01:44 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,12 @@ void print_rev(char *s)
 
 	int b;
 
-	for (inicial = 0; str[inicial] != '\0'; inicial++)
+	for (inicial = 0; s[inicial] != '\0'; inicial++)
 	{
 	}
 	for (b = inicial - 1; b >= 0; b--)
 	{
-		_putchar(str[b]);
+		_putchar(s[b]);
 	}
 	_putchar('\n');
 }
