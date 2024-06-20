c7d3f185f10f0f6ad2f1aac5082b6f525774db3f
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-20 14:12:27 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - cuenta los caracteres de la array
+ *
+ * @lent: almacena los numeros contados
+ * Return: retona la cantidad de los caracteres de la cadena
+ */  
+int _strlen(char *s)
+{
+	int lent;
+
+	for (len = 0; s[len]!= '\0'; len++)
+	{
+		return(len);
+	}
+}
