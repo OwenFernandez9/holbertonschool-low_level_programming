2e3c06fe4de39f92ad4fb474e55987d4cf9843d6
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-24 23:07:20 +0000

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -15,7 +15,7 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; src[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; dest[j] != '\0'; j++)
+	for (j = 0; dest[j] != '\0'; j++, i++)
 	{
 		src[i] = dest[j];
 	}
