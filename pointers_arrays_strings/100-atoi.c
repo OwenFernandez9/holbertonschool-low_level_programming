74bb2173140dfa2e7c28cc430dbee0a3eeff10e6
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-24 20:32:56 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,29 @@
 #include "main.h"
+/**
+ */
+int _atoi(char *s)
+{
+	int i = 0, m = -1, p = 0;
+	int viElNumero = 0, resultado = 0, contMenos = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (viElNumero)
+		{
+			if (s[i] no es un numero)
+				rompo;
+			
+
+
+		}
+		else
+		{
+			if (s[i] == '-')
+				contMenos = contMenos + 1
+		}
+
+		if (s[i] es un numero)
+			viElNumero = 1;
+
+	}
+}
