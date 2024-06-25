74af5afe5f9322e47bb4e945b66da4a035803bb7
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-25 00:08:09 +0000

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcmp - compara dos strings
+ *
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i]!= '\0' && s2[i]!= '\0'; i++)
+	{
+		if (s1[i]!= s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+
+	if (s1[i] == '\0' && s2[i]!= '\0')
+	{
+		return (-s2[i]);
+	}
+	if (s1[i]!= '\0' && s2[i] == '\0')
+	{
+		return (s1[i]);
+	}
+	return (0);
+}
