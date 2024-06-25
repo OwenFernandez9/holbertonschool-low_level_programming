6f873105538563fd706cb52c00d809a4805b742f
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-25 23:34:32 +0000

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
+ * _memcpy - aaa
+ *
+ * @dest: 1
+ * @src: 2
+ * @n: n
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-    for (i = 0; i < n; i++) {
-        *(dest + i) = *(src + i);
+	for (i = 0; i < n; i++) {
+	    *(dest + i) = *(src + i);
     }
-
     return dest;
-}
 }
