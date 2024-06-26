3e88eceb9c3ab8c55c3e6fb1200cb3d65ed71e48
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-26 19:24:24 +0000

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -2,17 +2,20 @@
 /**
  * _print_rev_recursion - imprime la cadena alrevez
  *
- * @s 
+ * @s: s
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s <='\0')
+	if (*s == '\0')
 	{
 		--s;
 		_putchar(*s);
-		_puts_recursion(s + 1);
-		return;
+
 	}
+
+	else if (*s == 0)
+		return;
+
 	else
 		_putchar('\n');
 }
