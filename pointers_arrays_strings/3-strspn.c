b7d0016b2926dc1880538e421ce95a6a2ab8047a
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-26 00:16:30 +0000

    nose

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - aaaaa
+ *
+ * @s: s
+ * @accept: accept
+ *
+ * Return: count
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
+		}
+	}
+	return (count);
+}
