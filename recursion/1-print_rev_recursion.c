b0df17e6c6c1d2d1e6862ce80b6dde9689a11523
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-26 14:36:11 +0000

    1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - aaa
+ *
+ * @s: string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		s--;
+		_putchar(*s);
+		_puts_recursion(s - 1);
+	}
+	else
+		_putchar('\n');
+
+}
