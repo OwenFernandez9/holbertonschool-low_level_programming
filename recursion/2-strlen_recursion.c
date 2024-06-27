7560bfceb00aa658cf0ae43d6f0fcfed11b83a5c
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-27 13:58:04 +0000

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,20 +2,13 @@
 /**
  * _strlen_recursion - aaaa
  *
- * _strlen_aux - bbbbb
- *
  * @s: s
- * @len: longitud
  * Return: Return
  */
-int _strlen_aux(char *s, int len)
-{
-	if (*s == '\0')
-		return (len);
-	else
-		return (_strlen_aux(s + 1, len + 1));
-}
 int _strlen_recursion(char *s)
 {
-	return (_strlen_aux(s, 0));
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen_recursion(s + 1));
 }
