c85d0e796799aa4342b1b42b522d30cdf0ba7a21
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-28 13:23:05 +0000

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,12 +1,32 @@
 #include "main.h"
 /**
+ * _sqrt_aux - bbb
+ *
+ * @x: x
+ * @n: n
+ * Return: retorno
+ */
+int _sqrt_aux(int x, int n)
+{
+	if (x > n)
+		return (-1);
+
+	else if (n != (x * x))
+		x++;
+	else
+		return (x);
+
+	return (_sqrt_aux(x, n));
+}
+/**
+ * _sqrt_recursion - aaaa
+ *
+ * @n: n
+ * Return: retorno
  */
 int _sqrt_recursion(int n)
 {
-	if ( n != (int)n)
-	{
-		return (- 1);
-	}
-	else
-		return _sqrt_recursion(n+ 1);
+	int x = 1;
+
+	return (_sqrt_aux(x, n));
 }
