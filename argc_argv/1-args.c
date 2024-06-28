8d322be17ffaefd4d6aa2627450feabcddfdba09
OwenFernandez9 <9357@holbertonstudents.com>
2024-06-28 19:21:54 +0000

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * @argc: argc
+ * @argv: unused
+ *
+ * Return: retorno
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
