db7e3843de05039326313aa6c4de3972b254d609
OwenFernandez9 <9357holbertonstudents.com>
2024-06-30 00:48:05 +0000

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
+ * main - aaaa
+ *
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
  */
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
-	
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
 }
