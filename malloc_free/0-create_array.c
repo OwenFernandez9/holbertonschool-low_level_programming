aa015add4d1f365ca0311ad34491d24c9cb27023
OwenFernandez9 <9357@holbertonstudents.com>
2024-07-02 14:28:34 +0000

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,10 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * create_array - aaaa
+ *
+ *@size: longitud de la array
+ *@c: c
+ *Return: retorno
  */
 char *create_array(unsigned int size, char c)
 {
-	char *a
-	
-	
+	char *a;
+	unsigned int i = 0;
+
+	a = malloc(size * sizeof(char));
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
+	}
+	return (a);
 }
