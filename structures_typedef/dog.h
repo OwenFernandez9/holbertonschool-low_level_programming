3263e5e07918815c2fbeab71f7902df41e933476
OwenFernandez9 <9357@holbertonstudents.com>
2024-07-09 20:05:26 +0000

    estruct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - dog
+ *
+ * @name: nombre
+ * @age: edad
+ * @owner: due√o
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
