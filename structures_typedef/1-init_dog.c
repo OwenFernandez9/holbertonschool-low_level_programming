ede1e8347dcee9d2908dbbd10d787c9ebbd865bf
OwenFernandez9 <9357@holbertonstudents.com>
2024-07-09 20:16:21 +0000

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - struct dog
+ * @d: puntero
+ * @name: nombre
+ * @age: edad
+ * @owner: owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
