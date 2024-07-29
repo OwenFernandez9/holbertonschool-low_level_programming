ca65b0d6825f09a355ecc8876424b7f0bc599ab6
OwenFernandez9 <9357@holbertonstudents.com>
2024-07-29 18:43:14 +0000

    retorno la cantidad de elementos

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stddef.h>
+/**
+ * list_len - list-len
+ * list_t - list_t
+ * @h: puntero de header
+ * Return: retorno count
+ */
 size_t list_len(const list_t *h)
 {
 	int count = 0;
