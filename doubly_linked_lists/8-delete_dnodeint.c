b75877615fb919e91309b08afacab5ee0fd450c0
OwenFernandez9 <9357@holbertonstudents.com>
2024-07-31 19:45:17 -0300

    Update delete

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,15 +10,4 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = (*head)->next;
 		count++;
 	}
-	if (*head != NULL)
-	{
-		(*head)->prev->next = (*head)->next;
-		(*head)->next->prev = (*head)->prev;
-
-		free (*head);
-		return(1);
-	}
-	else
-		return(-1);
-
 }
