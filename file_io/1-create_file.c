6573331d420e7926921b9523b8a6bebedc7742cc
OwenFernandez9 <9357@holbertonstudents.com>
2024-08-02 23:52:09 +0000

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -5,7 +5,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int f;
 
-	f = open(filename, O_WRONLY | O_CREAT, 0600);
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (f == -1)
 	{
 		return (-1);
