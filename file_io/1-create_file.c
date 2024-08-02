5cd25cc9c08818da8affe390aef037e539b286ea
OwenFernandez9 <9357@holbertonstudents.com>
2024-08-02 23:49:11 +0000

    create file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+
+	f = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (f == -1)
+	{
+		return (-1);
+	}
+	write(f, text_content,strlen(text_content));
+	close(f);
+	return (1);
+}
