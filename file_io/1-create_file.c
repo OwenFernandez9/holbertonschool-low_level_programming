018eebfb94f457fa98b3af8205d44da1fd157bf6
OwenFernandez9 <9357@holbertonstudents.com>
2024-08-02 23:55:24 +0000

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
 /**
+ * create_file - create file
+ *
+ * @filename: filename
+ * @text_content - aa
+ * Return: 1
  */
 int create_file(const char *filename, char *text_content)
 {
