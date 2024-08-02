4ec805684c327f4221f5b03396c2f594f72a5383
OwenFernandez9 <9357@holbertonstudents.com>
2024-08-02 21:40:51 +0000

    insert in idx

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (i->next == NULL && count <= idx)
 	{
-        	return(add_dnodeint_end(h, n));
+		return (add_dnodeint_end(h, n));
 	}
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
