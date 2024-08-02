32963a668fa875aa270616465466af4d21508dba
OwenFernandez9 <9357@holbertonstudents.com>
2024-08-02 23:21:57 +0000

    tools

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f = 0, count = 0;
+	char *buff;
+
+	f = open(filename, O_RDONLY);
+	if (f == -1)
+	{
+		return (0);
+	}
+	buff = malloc(letters);
+	if (buff == NULL)
+	{
+		return (0);
+	}
+	count = read(f, buff, letters);
+	count = write(2, buff, count);
+	close(f);
+	free(buff);
+	return (count);
+}
