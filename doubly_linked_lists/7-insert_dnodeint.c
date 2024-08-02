d4ac33436cd08eeab7eab4fdf451ba7e9aaa33f5
OwenFernandez9 <9357@holbertonstudents.com>
2024-08-02 14:25:53 +0000

    insert in idx

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	else
 	{
-		add_dnodeint_end(&backup, n)
+		add_dnodeint_end(&backup, n);
 	}
 	(*h)->next = newnode;
 	newnode->n = n;
